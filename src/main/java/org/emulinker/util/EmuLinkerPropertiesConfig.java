3c66d470342d5018b6b4595fd79160d7a039a325
Aleksandr Pavlov <ckidoz@gmail.com>
2026-01-02 00:32:20 +0300

    Fix config

--- src/main/java/org/emulinker/util/EmuLinkerPropertiesConfig.java
+++ src/main/java/org/emulinker/util/EmuLinkerPropertiesConfig.java
@@ -1,12 +1,15 @@
 package org.emulinker.util;
 
 import org.apache.commons.configuration2.PropertiesConfiguration;
+import org.apache.commons.configuration2.convert.DefaultListDelimiterHandler;
 import org.apache.commons.configuration2.ex.ConfigurationException;
 import org.apache.commons.configuration2.io.FileHandler;
 
 public class EmuLinkerPropertiesConfig extends PropertiesConfiguration {
     public EmuLinkerPropertiesConfig() throws ConfigurationException {
         super();
+        // Enable comma-separated list parsing (required in Commons Configuration 2.x)
+        setListDelimiterHandler(new DefaultListDelimiterHandler(','));
         FileHandler handler = new FileHandler(this);
         handler.load(EmuLinkerPropertiesConfig.class.getResource("/application.properties"));
         setThrowExceptionOnMissing(true);
